7af0336b7a52172c18e31b54d7ab3c2cdc29da3a
Allen Lin <chilly37647@gmail.com>
2023-04-14 00:11:49 +0800

    add files

---
+++ script.js
@@ -0,0 +1,266 @@
+const quoteContainer = document.querySelector("#quote-container");
+const quoteText = document.querySelector("#quote");
+const authorText = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const newQuoteBtn = document.querySelector("#new-quote");
+
+const loader = document.querySelector("#loader");
+
+
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details')
+const error404 = document.querySelector('.not-found');
+
+
+
+///////////weather APP/////////////////////////////////////////////////////////
+
+search.addEventListener('click',() =>{
+    const APIkey = APIKEY;
+    const city = document.querySelector('.search-box input').value
+
+    if(city ==='') return;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`
+    ).then(response => response.json()).then(json => {
+        if (json.cod === '404'){
+            container.style.height = '350px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+            return;
+        }
+
+        error404.style.display = 'none';
+        error404.classList.remove('fadeIn');
+
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector(".weather-box .description");
+        const wind = document.querySelector('.weather-details .wind span');
+        const humidity = document.querySelector('.weather-details .humidity span')
+
+
+        switch (json.weather[0].main) {
+          case "Clear":
+            image.src = "images/clear.png";
+            break;
+          case "Rain":
+            image.src = "images/rain.png";
+            break;
+          case "Snow":
+            image.src = "images/snow.png";
+            break;
+          case "Clouds":
+            image.src = "images/cloud.png";
+            break;
+          case "Haze":
+            image.src = "images/haze.png";
+            break;
+
+          default:
+            image.src = '';
+        }
+
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+
+        weatherBox.style.display = '';
+        weatherDetails.style.display = '';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '350px';
+
+
+    });
+});
+
+
+
+
+//Define apiQuotes as Array
+// let apiQuotes = [];
+
+//Show New Quotes/////////////////////////////////////////////////////
+//we can also use localQuotes by replacing apiQuotes to LocalQuotes
+function newQuote() {
+  loading();
+  //get random quote
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  //if quote too long, smaller
+  if (quote.quote.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.quote;
+  // put ont the author~
+  const author = quote.author ? quote.author : "Unknown";
+  authorText.textContent = author;
+  complete();
+}
+
+newQuoteBtn.addEventListener('click',newQuote);
+
+
+//Get Quotes Form API////////////////////////////////////////////////
+// async function getQuotes() {
+//   loading();
+//   const apiUrl = "https://type.fit/api/quotes";
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {}
+// }
+
+//Tweet Quote!///////////////////////////////////////////////////////
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+
+twitterBtn.addEventListener('click',tweetQuote);
+
+///////////Loader~~~//////////////////////////////////////////////
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+///////////////On Load!!!!//////////////////////////////////
+// getQuotes();
+newQuote();
+// loading()
+particlesJS(
+  "particles-js",
+
+  {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+    config_demo: {
+      hide_card: false,
+      background_color: "#b61924",
+      background_image: "",
+      background_position: "50% 50%",
+      background_repeat: "no-repeat",
+      background_size: "cover",
+    },
+  }
+);
